55dd74c58dab0f687161925ad090497850abb20f
bruno <akmacicbruno@gmail.com>
2022-02-14 00:20:58 +0100

    U funkciju ubačen kod za automatsko dodjeljivanje šifre, dodan progressBar

--- app/src/main/java/com/example/lijekovi/NewMedicineActivity.java
+++ app/src/main/java/com/example/lijekovi/NewMedicineActivity.java
@@ -7,12 +7,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -41,7 +43,8 @@ public class NewMedicineActivity extends AppCompatActivity {
     private ImageView slika;
     private String sCurrentUser;
     private String lastChild;
-    private Integer newKey;
+    private Integer newKey, newKeyLijek;
+    private ProgressBar pb_newMed;
 
     int SELECT_PICTURE = 200;
 
@@ -58,12 +61,15 @@ public class NewMedicineActivity extends AppCompatActivity {
         databaseReferenceuserandmed = FirebaseDatabase.getInstance().getReference("korisnikov_lijek");
 
         ime = (EditText) findViewById(R.id.editText_newMed_name);
-        sifra = (EditText) findViewById(R.id.editText_newMed_code);
+//        sifra = (EditText) findViewById(R.id.editText_newMed_code);
         proizvodac = (EditText) findViewById(R.id.editText_newMed_producer);
         kolicina = (EditText) findViewById(R.id.editText_newMed_quantity);
         dani = (EditText) findViewById(R.id.editText_newMed_days);
         vrijeme = (EditText) findViewById(R.id.editText_newMed_time);
         slika = (ImageView) findViewById(R.id.imageView_medNew_lijek);
+        pb_newMed = (ProgressBar) findViewById(R.id.progressBar_newMed);
+
+        pb_newMed.setVisibility(View.INVISIBLE);
 
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
@@ -113,9 +119,6 @@ public class NewMedicineActivity extends AppCompatActivity {
 
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
-
-
         if (requestCode == ImageBack) {
 
             // compare the resultCode with the
@@ -126,6 +129,7 @@ public class NewMedicineActivity extends AppCompatActivity {
                 btn_dodajLijek.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
+                        pb_newMed.setVisibility(View.VISIBLE);
                         StorageReference imgName = storageReference.child("med"+ selectedImageUri.getLastPathSegment());
                         imgName.putFile(selectedImageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
                             @Override
@@ -135,9 +139,8 @@ public class NewMedicineActivity extends AppCompatActivity {
                                     @Override
                                     public void onSuccess(Uri uri) {
                                         Uri downloadUrl = uri;
-                                        Toast.makeText(NewMedicineActivity.this, "Image uploaded!" + downloadUrl, Toast.LENGTH_SHORT).show();
                                         sIme = ime.getText().toString();
-                                        sSifra = sifra.getText().toString();
+//                                        sSifra = sifra.getText().toString();
                                         sProizvodac = proizvodac.getText().toString();
                                         sKolicina = kolicina.getText().toString();
                                         sDani = dani.getText().toString();
@@ -145,54 +148,61 @@ public class NewMedicineActivity extends AppCompatActivity {
                                         sSlikaUrl = downloadUrl.toString();
 
                                         Log.d("TAGslikaurl", "onClick: " + sSlikaUrl);
+                                        databaseReference.limitToLast(1).addListenerForSingleValueEvent(new ValueEventListener() {
+                                            @Override
+                                            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                                for (DataSnapshot childSnapshotLijek: snapshot.getChildren()) {
+                                                    lastChild = childSnapshotLijek.getKey().toString();
+                                                }
+                                                newKeyLijek = (Integer.parseInt(lastChild)+10);
+                                                if (TextUtils.isEmpty(sIme) || TextUtils.isEmpty(sProizvodac) || TextUtils.isEmpty(sKolicina) || TextUtils.isEmpty(sDani) || TextUtils.isEmpty(sVrijeme)) {
+                                                    pb_newMed.setVisibility(View.INVISIBLE);
+                                                    Toast.makeText(NewMedicineActivity.this, "All fields required!", Toast.LENGTH_SHORT).show();
+                                                }
+                                                else {
+                                                    Medicine new_med = new Medicine(newKeyLijek.toString(), sIme, sProizvodac, sDani, sVrijeme, sKolicina, sSlikaUrl);
+                                                    databaseReference.child(newKeyLijek.toString()).setValue(new_med);
 
-                                        Medicine new_med = new Medicine(sSifra, sIme, sProizvodac, sDani, sVrijeme, sKolicina, sSlikaUrl);
-                                        databaseReference.child(sSifra).setValue(new_med);
+                                                    databaseReferenceuserandmed.limitToLast(1).addListenerForSingleValueEvent(new ValueEventListener() {
+                                                        @Override
+                                                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                                            for (DataSnapshot childSnapshot: snapshot.getChildren()) {
+                                                                lastChild = childSnapshot.getKey().toString();
+                                                            }
+                                                            Log.d("TAGtest", "onDataChange: " + lastChild);
+                                                            newKey = (Integer.parseInt(lastChild)+1);
+                                                            Log.d("Nesto", "onChildAdded: " + lastChild + "---->" + newKey.toString());
+
+                                                            Map<String, String> korisnik_lijek = new HashMap<>();
+                                                            korisnik_lijek.put("id", newKey.toString());
+                                                            korisnik_lijek.put("korisnik", sCurrentUser);
+                                                            korisnik_lijek.put("lijek", newKeyLijek.toString());
+                                                            Log.d("TAGtest", "newkey: " + newKey.toString());
+                                                            databaseReferenceuserandmed.child(newKey.toString()).setValue(korisnik_lijek);
+
+                                                            pb_newMed.setVisibility(View.INVISIBLE);
+                                                            Toast.makeText(NewMedicineActivity.this, "Successfully added new medicine!", Toast.LENGTH_LONG).show();
+                                                            Intent intent = new Intent(NewMedicineActivity.this, HomeActivity.class);
+                                                            startActivity(intent);
+                                                            finish();
+                                                        }
+
+                                                        @Override
+                                                        public void onCancelled(@NonNull DatabaseError error) {
+                                                            pb_newMed.setVisibility(View.INVISIBLE);
+                                                        }
+                                                    });
+                                                }
+                                            }
+                                            @Override
+                                            public void onCancelled(@NonNull DatabaseError error) {
+                                                pb_newMed.setVisibility(View.INVISIBLE);
+                                            }
+                                        });
                                     }
                                 });
                             }
 
-                        });
-//                        sIme = ime.getText().toString();
-                        sSifra = sifra.getText().toString();
-//                        sProizvodac = proizvodac.getText().toString();
-//                        sKolicina = kolicina.getText().toString();
-//                        sDani = dani.getText().toString();
-//                        sVrijeme = vrijeme.getText().toString();
-//                        sSlikaUrl = imgName.getDownloadUrl().toString();
-//
-//                        Log.d("TAGslikaurl", "onClick: " + sSlikaUrl);
-//
-//                        Medicine new_med = new Medicine(sSifra, sIme, sProizvodac, sDani, sVrijeme, sKolicina, sSlikaUrl);
-//                        databaseReference.child(sSifra).setValue(new_med);
-
-                        databaseReferenceuserandmed.limitToLast(1).addListenerForSingleValueEvent(new ValueEventListener() {
-                            @Override
-                            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                                for (DataSnapshot childSnapshot: snapshot.getChildren()) {
-                                    lastChild = childSnapshot.getKey().toString();
-                                }
-                                Log.d("TAGtest", "onDataChange: " + lastChild);
-                                newKey = (Integer.parseInt(lastChild)+1);
-                                Log.d("Nesto", "onChildAdded: " + lastChild + "---->" + newKey.toString());
-
-                                Map<String, String> korisnik_lijek = new HashMap<>();
-                                korisnik_lijek.put("id", newKey.toString());
-                                korisnik_lijek.put("korisnik", sCurrentUser);
-                                korisnik_lijek.put("lijek", sSifra);
-                                Log.d("TAGtest", "newkey: " + newKey.toString());
-                                databaseReferenceuserandmed.child(newKey.toString()).setValue(korisnik_lijek);
-
-                                Toast.makeText(NewMedicineActivity.this, "Successfully added new medicine!", Toast.LENGTH_LONG).show();
-                                Intent intent = new Intent(NewMedicineActivity.this, HomeActivity.class);
-                                startActivity(intent);
-                                finish();
-                            }
-
-                            @Override
-                            public void onCancelled(@NonNull DatabaseError error) {
-
-                            }
                         });
                     }
                 });
